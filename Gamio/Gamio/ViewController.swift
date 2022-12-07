863f77bd68dc92c1ece69fdcc82c4312bfbb195f
ozcgokberk <ozcangokberk06@gmail.com>
2022-12-07 15:28:15 +0300

    -initial commit of Gamio Project.

---
+++ Gamio/Gamio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gamio
+//
+//  Created by Gokberk Ozcan on 7.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
